48fb9cdcb75cd30f962db4a813736de5d088b4e0
EstebanAlarconO <esteban.alarcon1@mail.udp.cl>
2022-05-20 00:06:35 -0400

    Avance

--- Producer/api_producer.py
+++ Producer/api_producer.py
@@ -1,11 +1,25 @@
 import json
-from time import sleep, localtime
+from time import sleep, ctime, time
 from flask import Flask, render_template, request
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer
+from aiokafka import AIOKafkaProducer
+import asyncio
 
 app = Flask(__name__)
 topic_list = []
 
+
+async def send_one(message):
+    producer = AIOKafkaProducer(
+        bootstrap_servers='kafka:9092')
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    try:
+        # Produce message
+        await producer.send_and_wait("test", message)
+    finally:
+        # Wait for all pending messages to be delivered or expire.
+        await producer.stop()
 #producer = KafkaProducer()
 @app.route('/')
 def index():
@@ -15,9 +29,11 @@ def index():
 def login():
     if request.method == 'POST':
         user = request.form['user']
-        producer = KafkaProducer(bootstrap_servers=['kafka:9092'], value_serializer=lambda x: json.dumps(x).encode('ascii'))
-        producer.send('test', {'user':user, 'hora_login': localtime()})
-        producer.flush()
+        message={'user': user, 'time_login': time()}
+        message = json.dumps(message).encode('utf-8')
+        asyncio.run(send_one(message))
+        '''producer = KafkaProducer(bootstrap_servers=['kafka:9092'], value_serializer=lambda x: json.dumps(x).encode('UTF-8'))
+        producer.send('test', {'user':user, 'hora_login': localtime()})'''
         #producer.send('test', key=b'message-two', value=b'This is Kafka-Python')
     return render_template('index.html')
     
